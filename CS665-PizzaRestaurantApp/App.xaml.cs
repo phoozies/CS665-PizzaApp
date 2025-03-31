07e38e518e7fad8d22f4fdf76e66b63c96e96723
phoozies <tpvo.personal@gmail.comgit config --global user.name>
2025-03-30 23:28:28 -0500

    Add project files.

---
+++ CS665-PizzaRestaurantApp/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CS665_PizzaRestaurantApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
